d28de723c86e17a6ce912a3e131f2337e4bc73eb
Алексей Игоревич <skobelkin.97@inbox.ru>
2025-07-08 18:34:53 +0300

    Изменил exception

--- main/src/main/java/ru/practicum/ewm/exeption/GlobalExceptionHandler.java
+++ main/src/main/java/ru/practicum/ewm/exeption/GlobalExceptionHandler.java
@@ -1,9 +1,14 @@
 package ru.practicum.ewm.exeption;
 
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolationException;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,7 +19,9 @@ import java.util.Arrays;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({NumberFormatException.class,
-            DataIntegrityViolationException.class})
+            DataIntegrityViolationException.class,
+            ConstraintViolationException.class,
+            MethodArgumentNotValidException.class})
     public ResponseEntity<ApiError> handleNumberFormatException(Exception e) {
         ApiError error = ApiError.builder()
                 .errors(
@@ -30,36 +37,22 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler({
+            ForbiddenException.class,
             UserAlreadyExistsByEmail.class,
-            CategoryAlreadyExists.class
+            CategoryAlreadyExists.class,
+            EntityNotFoundException.class
     })
-    public ResponseEntity<ApiError> handleEntityAlreadyExist(Exception e) {
+    public ResponseEntity<ApiError> handleForbiddenException(ForbiddenException e) {
         ApiError error = ApiError.builder()
                 .errors(
                         Arrays.stream(e.getStackTrace())
                                 .map(StackTraceElement::toString)
                                 .toList()
                 ).message(e.getMessage())
-                .reason("Entity already exists.")
+                .reason("FORBIDDEN.")
                 .status(HttpStatus.CONFLICT.toString())
                 .timestamp(LocalDateTime.now().toString()).build();
 
         return new ResponseEntity<>(error, HttpStatus.CONFLICT);
     }
-
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ApiError> handleEntityNotFoundException(EntityNotFoundException e) {
-        ApiError error = ApiError.builder()
-                .errors(
-                        Arrays.stream(e.getStackTrace())
-                                .map(StackTraceElement::toString)
-                                .toList()
-                ).message(e.getMessage())
-                .reason("Entity not found.")
-                .status(HttpStatus.INTERNAL_SERVER_ERROR.toString())
-                .timestamp(LocalDateTime.now().toString()).build();
-
-        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
 }
