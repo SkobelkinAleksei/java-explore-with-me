3251660770c50d8b9aa0cc87a6e1bd65986809a4
Lores <Lorestatys@yandex.ru>
2025-07-15 16:59:23 +0300

    Исправил замечания

--- stats/stats-server/src/main/test/java/ru/practicum/statsserver/StatsServerApplicationTests.java
+++ stats/stats-server/src/main/test/java/ru/practicum/statsserver/StatsServerApplicationTests.java
@@ -1,14 +1,14 @@
-//package ru.practicum.statsserver;
-//
-//import org.junit.jupiter.api.Disabled;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//@Disabled("Тесты временно отключены")
-//public class StatsServerApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//}
+package ru.practicum.statsserver;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled("Тесты временно отключены")
+public class StatsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
